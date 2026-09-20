d44e89f2456f3e426296976cde04c40437919d20
TorntornSahur <arcszanelle@gmail.com>
2026-09-20 23:15:10 +0800

    Delete DEF_ACTIVITY.py

--- DEF_ACTIVITY.py
+++
@@ -1,9 +0,0 @@
-def call_out():
-    print("HELLO! This is my example of a def function")
-    
-    
-call_out()
-
-#Def is a keyword in Python used to create function. 
-#It is used to recall a block of code to do a specific task.
-#Function can make coding much easier since you dont need to retype a code, you just call it.
