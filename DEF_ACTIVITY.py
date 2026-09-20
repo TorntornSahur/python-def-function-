4d587692c9f2f3a17f730307a973938e8ed06e34
TorntornSahur <arcszanelle@gmail.com>
2026-09-20 22:51:56 +0800

    Add files via upload

---
+++ DEF_ACTIVITY.py
@@ -0,0 +1,9 @@
+def call_out():
+    print("HELLO! This is my example of a def function")
+    
+    
+call_out()
+
+#Def is a keyword in Python used to create function. 
+#It is used to recall a block of code to do a specific task.
+#Function can make coding much easier since you dont need to retype a code, you just call it.
