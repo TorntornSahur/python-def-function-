8507def8aafde3b54cc0581ac5ff46930fbe5271
TorntornSahur <arcszanelle@gmail.com>
2026-09-20 23:15:26 +0800

    Add files via upload

---
+++ DEF_ACTIVITY.py
@@ -0,0 +1,12 @@
+def inches_to_cm(inches):
+    return inches * 2.54
+
+inches = float(input("What's your inch?: "))
+result = inches_to_cm(inches)
+
+print(result)   
+
+
+#Def is a keyword in Python used to create function. 
+#It is used to recall a block of code to do a specific task.
+#Function can make coding much easier since you dont need to retype a code, you just call it.12 
